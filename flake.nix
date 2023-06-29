e2649e53e204ec5d8af60a3b0c38f860f6cf98e8
Stefano Baghino <stefano@baghino.me>
2023-06-29 15:24:56 +0200

    add formatter, apps, lock

--- flake.nix
+++ flake.nix
@@ -6,27 +6,39 @@
     flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = with pkgs; stdenv.mkDerivation {
-          name = "maelstrom";
-          version = "0.2.3";
-          src = fetchzip {
-            url = "https://github.com/jepsen-io/maelstrom/releases/download/v0.2.3/maelstrom.tar.bz2";
-            hash = "sha256-mE/FIHDLYd1lxAvECZGelZtbo0xkQgMroXro+xb9bMI=";
-          };
-          buildInputs = [ makeWrapper ];
-          installPhase = ''
-            mkdir -p $out/bin $out/lib
-            cp lib/maelstrom.jar $out/lib/maelstrom.jar
-            makeWrapper ${jre}/bin/java $out/bin/maelstrom --add-flags "-Djava.awt.headless=true -jar $out/lib/maelstrom.jar"
-          '';
-          propagatedBuildInputs = [ graphviz gnuplot ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in rec {
+        packages = {
+          default = packages.maelstrom;
+          maelstrom = with pkgs;
+            stdenv.mkDerivation {
+              name = "maelstrom";
+              version = "0.2.3";
+              src = fetchzip {
+                url = "https://github.com/jepsen-io/maelstrom/releases/download/v0.2.3/maelstrom.tar.bz2";
+                hash = "sha256-mE/FIHDLYd1lxAvECZGelZtbo0xkQgMroXro+xb9bMI=";
+              };
+              buildInputs = [makeWrapper];
+              installPhase = ''
+                mkdir -p $out/bin $out/lib
+                cp lib/maelstrom.jar $out/lib/maelstrom.jar
+                makeWrapper ${jre}/bin/java $out/bin/maelstrom --add-flags "-Djava.awt.headless=true -jar $out/lib/maelstrom.jar"
+              '';
+              propagatedBuildInputs = [graphviz gnuplot];
+            };
         };
+        apps = rec {
+          default = apps.maelstrom;
+          maelstrom = flake-utils.lib.mkApp {drv = packages.default;};
+        };
+        formatter = pkgs.alejandra;
       }
     );
 }
